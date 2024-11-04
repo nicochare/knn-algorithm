517d06a35f422875c7a9840026b5e1a58d5b2369
Nicolás Chareca <55698485+nicochare@users.noreply.github.com>
2024-11-04 17:26:09 +0100

    asd

--- main.c
+++ main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
+    FILE *myFile = NULL;
     
-    return 0;
+    myFile = fopen("diabetes_prediction_dataset", "r");
+    if (myFile == NULL) {
+        printf("\nError de apertura de archivo.\n");
+        exit(-1);
+    } else {
+        char buffer[80];
+        while (fgets(buffer, 80, myFile)) {
+            // If you need all the values in a row
+            char *token = strtok(buffer, ",");
+            while (token) { 
+                // Just printing each integer here but handle as needed
+                int n = atoi(token);
+                printf("%d\n", n);
+
+                token = strtok(NULL, ",");
+            }
+        }
+        return 0;
+    }
 }

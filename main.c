3ce288eeaa309501a9c52eb0b4be0695074a8152
Nicolás Chareca <55698485+nicochare@users.noreply.github.com>
2024-11-04 17:40:14 +0100

    lectura_datos

--- main.c
+++ main.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAXLINE 100
 
 int main() {
     FILE *myFile = NULL;
     
-    myFile = fopen("diabetes_prediction_dataset", "r");
+    myFile = fopen("diabetes_prediction_dataset.csv", "r");
     if (myFile == NULL) {
         printf("\nError de apertura de archivo.\n");
         exit(-1);
     } else {
-        char buffer[80];
-        while (fgets(buffer, 80, myFile)) {
+        char buffer[MAXLINE];
+        fgets(buffer, MAXLINE, myFile);
+
+        while (fgets(buffer, MAXLINE, myFile)) {
             // If you need all the values in a row
             char *token = strtok(buffer, ",");
             while (token) { 
-                // Just printing each integer here but handle as needed
-                int n = atoi(token);
-                printf("%d\n", n);
+                printf("%s - ", token);
 
                 token = strtok(NULL, ",");
             }
+            printf("\n");
         }
         return 0;
     }

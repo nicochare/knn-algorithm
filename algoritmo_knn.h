a9c5f6132eac147ec4161d0df87bd978d2c73a9c
Pablox8 <pcarro90@gmail.com>
2024-11-18 18:07:34 +0100

    .h actualizado

--- algoritmo_knn.h
+++ algoritmo_knn.h
@@ -13,4 +13,4 @@ typedef struct registro {
 void normalizar_registro(Registro*);
 float calcular_distancia_registros(Registro, Registro);
 int algoritmo_knn(tipoMinMonticulo, int);
-void algoritmo_enn(tipoMinMonticulo*, int);
+void algoritmo_enn(tipoMinMonticulo, int, tipoMinMonticulo*);

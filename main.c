7c70f662e30a1ae81209162df5aa19b4e9cd4bd5
Nicolás Chareca <55698485+nicochare@users.noreply.github.com>
2024-12-02 12:03:31 +0100

    usar cantidad elementos variable

--- main.c
+++ main.c
@@ -2,14 +2,13 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
-#define TAMANIODATASET 100000
 #define RUTA "diabetes_prediction_dataset.csv"
 
-int obtener_k() {
+int obtener_k(int max) {
     int k = 0;
-    while (k < 1 || k > TAMANIODATASET) {
-        printf("1 <= k <= %d\n", TAMANIODATASET);
-        printf("Por favor, introduce el numero de registros con los que desea comparar (k): ");
+    while (k < 1) {
+        printf("(1 <= k <= %d\n", max);
+        printf("\nPor favor, introduce el numero de registros con los que desea comparar (k): ");
         scanf("%d", &k);
     }
     return k;
@@ -26,7 +25,7 @@ int main() {
 
     // Apartado 3
     tipoMaxMonticulo mm;
-    nuevoMaxMonticulo(&mm, TAMANIODATASET);
+    nuevoMaxMonticulo(&mm, devolverCantidadElementos(dataset));
 
     // Apartado 4
     printf("\n\nClasificación de un ejemplo nuevo mediante K-NN para K=1\n");
@@ -80,7 +79,7 @@ int main() {
     printf("    - Niveles HbA1c:       7.4\n");
     printf("    - Niveles Glucosa:     162\n\n");
     
-    k = obtener_k();
+    k = obtener_k(devolverCantidadElementos(dataset));
     
     reg_buscado = nuevo_registro(0.0, 81.0, 1.0, 1.0, 5.0, 26.92, 7.4, 162.0); // diabetes
     normalizar_registro(&reg_buscado);    
@@ -97,7 +96,7 @@ int main() {
     conjuntoDeEjemplos[3] = nuevo_registro(1.0, 53.0, 0.0, 0.0, 4.0, 26.81, 6.7, 158.0); // no diabetes
     conjuntoDeEjemplos[4] = nuevo_registro(1.0, 10.0, 0.0, 0.0, 3.0, 28.86, 6.1, 93.0); // no diabetes
 
-    k = obtener_k();
+    k = obtener_k(devolverCantidadElementos(dataset));
     
     i = 0;
     while (i < 5) {

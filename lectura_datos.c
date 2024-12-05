6642f1c0cec2bd1e82fdfe07b77b2b506b46be06
Nicolás Chareca <55698485+nicochare@users.noreply.github.com>
2024-12-05 18:43:57 +0100

    lectura csv nuevo para datos de conjunto de entradas

--- lectura_datos.c
+++ lectura_datos.c
@@ -1,14 +1,32 @@
 #include "lectura_datos.h"
+#include <string.h>
 #include <stdlib.h>
+
 #define MAXCARACTERESLINEA 100
 
 FILE* abrir_archivo(char* ruta) {
     FILE* fichero = NULL;
+    
+    char* dataset_path = "datasets/";
+    
+    size_t tam = strlen(ruta) + strlen(dataset_path) + 1;
+    char* ruta_completa = (char*)malloc(tam);
+    
+    strcpy(ruta_completa, dataset_path);
+    strcat(ruta_completa, ruta);    
 
-    if ((fichero = fopen("datasets/"+ruta, "r")) == NULL) {
-        printf("\nError de apertura de archivo.\n");
-        exit(-1);
+    if (ruta == NULL) {
+        printf("\nError al reservar memoria\n");
+        exit(1);
+    } else {
+        if ((fichero = fopen(ruta_completa, "r")) == NULL) {
+            printf("\nError de apertura de archivo.\n");
+            exit(-1);
+        }
     }
+
+    free(ruta_completa);
+
     return fichero;
 }
 
--- main.c
+++ main.c
@@ -51,22 +51,19 @@ int main() {
     // Apartado 5
     printf("\n\n\033[33m --- APARTADO 5: Clasificación de un conjunto de ejemplos mediante K-NN para K=1\033[0m\n");
     
-    Registro* conjuntoDeEjemplos = (Registro*)malloc(5*sizeof(Registro));
-    conjuntoDeEjemplos[0] = nuevo_registro(0.0, 64.0, 0.0, 0.0, 5.0, 31.7, 6.2, 162.0); // no diabetes
-    conjuntoDeEjemplos[1] = nuevo_registro(1.0, 36.0, 0.0, 0.0, 3.0, 55.91, 7.0, 151.0); // diabetes
-    conjuntoDeEjemplos[2] = nuevo_registro(0.0, 39.0, 0.0, 0.0, 3.0, 27.98, 5.9, 284.0); // diabetes
-    conjuntoDeEjemplos[3] = nuevo_registro(1.0, 53.0, 0.0, 0.0, 4.0, 26.81, 6.7, 158.0); // no diabetes
-    conjuntoDeEjemplos[4] = nuevo_registro(1.0, 10.0, 0.0, 0.0, 3.0, 28.86, 6.1, 93.0); // no diabetes
+    tipoCola dataset_ejemplos;
+    nuevaCola(&dataset_ejemplos);
+    cargar_en_cola("conjunto_knn.csv", &dataset_ejemplos);
+    normalizar_dataset(&dataset_ejemplos);
 
-    int i = 0;
-    while (i < 5) {
-        reg_buscado = conjuntoDeEjemplos[i];
+    while (!esNulaCola(dataset_ejemplos)) {
+        reg_buscado = frente(dataset_ejemplos);
         normalizar_registro(&reg_buscado);
 
         cargar_datos(&dataset, &mm, reg_buscado, 1);
         resultado = algoritmo_knn(&mm, 1);
         interpretacion_resultado(resultado);
-        i++;
+        desencolar(&dataset_ejemplos);
     }
 
     // Apartado 6
@@ -90,107 +87,24 @@ int main() {
 
     // Apartado 7
     printf("\n\n\033[33m --- APARTADO 7: Clasificación de un conjunto de ejemplos mediante K-NN para K=k\033[0m\n");
-    conjuntoDeEjemplos[0] = nuevo_registro(0.0, 64.0, 0.0, 0.0, 5.0, 31.7, 6.2, 162.0); // no diabetes
-    conjuntoDeEjemplos[1] = nuevo_registro(1.0, 36.0, 0.0, 0.0, 3.0, 55.91, 7.0, 151.0); // diabetes
-    conjuntoDeEjemplos[2] = nuevo_registro(0.0, 39.0, 0.0, 0.0, 3.0, 27.98, 5.9, 284.0); // diabetes
-    conjuntoDeEjemplos[3] = nuevo_registro(1.0, 53.0, 0.0, 0.0, 4.0, 26.81, 6.7, 158.0); // no diabetes
-    conjuntoDeEjemplos[4] = nuevo_registro(1.0, 10.0, 0.0, 0.0, 3.0, 28.86, 6.1, 93.0); // no diabetes
+    
+    cargar_en_cola("conjunto_knn.csv", &dataset_ejemplos);
+    normalizar_dataset(&dataset_ejemplos);
 
-    i = 0;
-    while (i < 5) {
-        reg_buscado = conjuntoDeEjemplos[i];
+    while (!esNulaCola(dataset_ejemplos)) {
+        reg_buscado = frente(dataset_ejemplos);
         normalizar_registro(&reg_buscado);
 
         cargar_datos(&dataset, &mm, reg_buscado, k);
-        resultado = algoritmo_knn(&mm, k);
+        resultado = algoritmo_knn(&mm, 1);
         interpretacion_resultado(resultado);
-        i++;
+        desencolar(&dataset_ejemplos);
     }
 
     // Apartado 8
     printf("\n\n\033[33m --- APARTADO 8: Pre-procesamiento del dataset mediante Algoritmo de Wilson (ENN) para K = %d\033[0m\n", k);
     algoritmo_enn(&dataset, k);
     
-    printf("REPETICION APARTADOS 6 y 7\n");
-    // Apartado 4
-    printf("\n\n\033[33m --- APARTADO 4: Clasificación de un ejemplo nuevo mediante K-NN para K=1\033[0m\n");
-    printf("Se utilizará un ejemplo de paciente con los siguientes datos:\n");
-    printf("    - Genero:              Hombre\n");
-    printf("    - Edad:                81\n");
-    printf("    - Hipertension:        Si\n");
-    printf("    - Enfermedad Cardíaca: Si\n");
-    printf("    - Historial fumador:   Actualmente no\n");
-    printf("    - BMI:                 26.92\n");
-    printf("    - Niveles HbA1c:       7.4\n");
-    printf("    - Niveles Glucosa:     162\n\n");
-    
-    reg_buscado = nuevo_registro(0.0, 81.0, 1.0, 1.0, 5.0, 26.92, 7.4, 162.0); // diabetes
-    normalizar_registro(&reg_buscado);
-    cargar_datos(&dataset, &mm, reg_buscado, 1);
-    resultado = algoritmo_knn(&mm, 1);
-    interpretacion_resultado(resultado);
-
-    // Apartado 5
-    printf("\n\n\033[33m --- APARTADO 5: Clasificación de un conjunto de ejemplos mediante K-NN para K=1\033[0m\n");
-    
-    conjuntoDeEjemplos = (Registro*)malloc(5*sizeof(Registro));
-    conjuntoDeEjemplos[0] = nuevo_registro(0.0, 64.0, 0.0, 0.0, 5.0, 31.7, 6.2, 162.0); // no diabetes
-    conjuntoDeEjemplos[1] = nuevo_registro(1.0, 36.0, 0.0, 0.0, 3.0, 55.91, 7.0, 151.0); // diabetes
-    conjuntoDeEjemplos[2] = nuevo_registro(0.0, 39.0, 0.0, 0.0, 3.0, 27.98, 5.9, 284.0); // diabetes
-    conjuntoDeEjemplos[3] = nuevo_registro(1.0, 53.0, 0.0, 0.0, 4.0, 26.81, 6.7, 158.0); // no diabetes
-    conjuntoDeEjemplos[4] = nuevo_registro(1.0, 10.0, 0.0, 0.0, 3.0, 28.86, 6.1, 93.0); // no diabetes
-
-    i = 0;
-    while (i < 5) {
-        reg_buscado = conjuntoDeEjemplos[i];
-        normalizar_registro(&reg_buscado);
-
-        cargar_datos(&dataset, &mm, reg_buscado, 1);
-        resultado = algoritmo_knn(&mm, 1);
-        interpretacion_resultado(resultado);
-        i++;
-    }
-
-    printf("\n\nClasificación de un ejemplo nuevo mediante K-NN para K=k\n");
-    printf("Se utilizará un ejemplo de paciente con los siguientes datos:\n");
-    printf("    - Genero:              Hombre\n");
-    printf("    - Edad:                81\n");
-    printf("    - Hipertension:        Si\n");
-    printf("    - Enfermedad Cardíaca: Si\n");
-    printf("    - Historial fumador:   Actualmente no\n");
-    printf("    - BMI:                 26.92\n");
-    printf("    - Niveles HbA1c:       7.4\n");
-    printf("    - Niveles Glucosa:     162\n\n");
-    
-    k = obtener_k(devolverCantidadElementos(dataset));
-    
-    reg_buscado = nuevo_registro(0.0, 81.0, 1.0, 1.0, 5.0, 26.92, 7.4, 162.0); // diabetes
-    normalizar_registro(&reg_buscado);    
-    
-    cargar_datos(&dataset, &mm, reg_buscado, k);
-    resultado = algoritmo_knn(&mm, k);
-    interpretacion_resultado(resultado);
-
-    // Apartado 7
-    printf("\n\nClasificación de un conjunto de ejemplos mediante K-NN para K=k\n");
-    conjuntoDeEjemplos[0] = nuevo_registro(0.0, 64.0, 0.0, 0.0, 5.0, 31.7, 6.2, 162.0); // no diabetes
-    conjuntoDeEjemplos[1] = nuevo_registro(1.0, 36.0, 0.0, 0.0, 3.0, 55.91, 7.0, 151.0); // diabetes
-    conjuntoDeEjemplos[2] = nuevo_registro(0.0, 39.0, 0.0, 0.0, 3.0, 27.98, 5.9, 284.0); // diabetes
-    conjuntoDeEjemplos[3] = nuevo_registro(1.0, 53.0, 0.0, 0.0, 4.0, 26.81, 6.7, 158.0); // no diabetes
-    conjuntoDeEjemplos[4] = nuevo_registro(1.0, 10.0, 0.0, 0.0, 3.0, 28.86, 6.1, 93.0); // no diabetes
-
-    
-    i = 0;
-    while (i < 5) {
-        reg_buscado = conjuntoDeEjemplos[i];
-        normalizar_registro(&reg_buscado);
-
-        cargar_datos(&dataset, &mm, reg_buscado, k);
-        resultado = algoritmo_knn(&mm, k);
-        interpretacion_resultado(resultado);
-        i++;
-    }
-
     
 
     return 0;
